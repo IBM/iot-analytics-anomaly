703f374068469fd3aa47e713791ab4fbed7371fc
Kalonji Bankol <kkbankol@us.ibm.com>
2020-03-13 18:20:16 -0700

    adding scripts

---
+++ scripts/invoke_model_function.py
@@ -0,0 +1,71 @@
+import json
+import logging
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, func
+from iotfunctions import bif
+from custom.functions import InvokeExternalModel
+from iotfunctions.metadata import EntityType
+from iotfunctions.db import Database
+from iotfunctions.base import BaseTransformer
+from iotfunctions.bif import EntityDataGenerator
+#from iotfunctions.enginelog import EngineLogging
+from custom import settings
+import datetime as dt
+
+import pandas as pd
+import numpy as np
+
+with open('credentials.json', encoding='utf-8') as F:
+    credentials = json.loads(F.read())
+
+'''
+Create a database object to access Watson IOT Platform Analytics DB.
+'''
+db = Database(credentials = credentials)
+db_schema = None #  set if you are not using the default
+
+# Credentials to access WML Model.
+WATSON_ML_ENDPOINT = settings.WATSON_ML_ENDPOINT
+WATSON_ML_MODEL_ID = settings.WATSON_ML_MODEL_ID
+WATSON_ML_APIKEY = settings.WATSON_ML_APIKEY
+WATSON_ML_DEPLOYMENT_ID = settings.WATSON_ML_DEPLOYMENT_ID
+
+IAM_UID = settings.IAM_UID
+IAM_PASSWORD = settings.IAM_PASSWORD
+
+MODEL_INPUT_COLUMNS = settings.MODEL_INPUT_COLUMNS
+
+
+if MODEL_INPUT_COLUMNS and (len(MODEL_INPUT_COLUMNS) > 0):
+    MODEL_INPUT_COLUMNS = MODEL_INPUT_COLUMNS.replace(' ', '').split(',')
+else:
+    MODEL_INPUT_COLUMNS = []
+
+entity_name = "kb_anomaly"
+
+entity = EntityType(entity_name, db,
+                    # following columns can be dynamically generated based on meters associated with each asset
+                    Column('deviceid',String(50)),
+                    # Column('evt_timestamp',String(50)),
+                    Column('anomaly_score', Integer()),
+                    Column("torque", Integer()),
+                    Column("acc", Integer()),
+                    Column("load", Integer()),
+                    Column("tool_type", Integer()),
+                    Column("speed", Float()),
+                    Column("travel_time", Float()),
+                    InvokeExternalModel(
+                                    # uid=IAM_UID,
+                                    # password=IAM_PASSWORD,
+                                    wml_endpoint=WATSON_ML_ENDPOINT,
+                                    model_id=WATSON_ML_MODEL_ID,
+                                    deployment_id=WATSON_ML_DEPLOYMENT_ID,
+                                    apikey=WATSON_ML_APIKEY,
+                                    input_features=MODEL_INPUT_COLUMNS,
+                                    output_item = 'anomaly_score_done'),
+                    **{
+                      '_timestamp' : 'evt_timestamp',
+                      '_db_schema' : db_schema}
+)
+job_settings = {'_production_mode': False}
+# entity.exec_local_pipeline(**job_settings)
+entity.exec_local_pipeline()
---
+++ scripts/register_entity.py
@@ -0,0 +1,68 @@
+import json
+import logging
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, func
+from iotfunctions import bif
+from custom.functions import InvokeExternalModel
+from iotfunctions.metadata import EntityType
+from iotfunctions.db import Database
+from iotfunctions.base import BaseTransformer
+from iotfunctions.bif import EntityDataGenerator
+#from iotfunctions.enginelog import EngineLogging
+from custom import settings
+import datetime as dt
+
+import pandas as pd
+import numpy as np
+#EngineLogging.configure_console_logging(logging.DEBUG)
+
+'''
+# Replace with a credentials dictionary or provide a credentials
+# Explore > Usage > Watson IOT Platform Analytics > Copy to clipboard
+# Past contents in a json file.
+'''
+with open('credentials.json', encoding='utf-8') as F:
+    credentials = json.loads(F.read())
+
+'''
+Developing Test Pipelines
+-------------------------
+When creating a set of functions you can test how they these functions will
+work together by creating a test pipeline.
+'''
+
+
+# t = BaseTransformer()
+'''
+Create a database object to access Watson IOT Platform Analytics DB.
+'''
+db = Database(credentials = credentials)
+db_schema = None #  set if you are not using the default
+
+entity_name = 'kb_anomaly'
+entity = EntityType(entity_name, db,
+                    Column('deviceid',String(50)),
+                    Column('anomaly_score', Integer()),
+                    Column("torque", Integer()),
+                    Column("acc", Integer()),
+                    Column("load", Integer()),
+                    Column("tool_type", Integer()),
+                    Column("speed", Float()),
+                    Column("travel_time", Float()),
+                    **{
+                      '_timestamp' : 'evt_timestamp',
+                      # '_production_mode': False,
+                      '_db_schema' : db_schema}
+)
+
+db.register_functions([InvokeExternalModel])
+print("Function registered")
+
+entity.register(raise_error=False)
+print("Entity registered")
+
+# generate data and set anomaly_score to zeros
+entity.generate_data(days=2.0, drop_existing=True)
+df = db.read_table(table_name=entity_name, schema=db_schema)
+df["anomaly_score"] = np.zeros(len(df))
+db.write_frame(df, table_name=entity_name, schema=db_schema, if_exists="replace")
+db.commit()
